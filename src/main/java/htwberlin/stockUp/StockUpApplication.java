800c0b55f5a2e8ca3f78b1242052acfb1f01369e
Melliim <ibrahim_meleha@hotmail.com>
2023-04-21 23:39:56 +0200

    add generated spring boot

---
+++ src/main/java/htwberlin/stockUp/StockUpApplication.java
@@ -0,0 +1,13 @@
+package htwberlin.stockUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/htwberlin/stockUp/StockUpApplicationTests.java
@@ -0,0 +1,13 @@
+package htwberlin.stockUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
